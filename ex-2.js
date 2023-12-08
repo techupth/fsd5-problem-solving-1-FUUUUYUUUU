ee060e52402e697a6f1e3c5e48fb97a7ba793cc5
Panupong <Pai28212m@gmail.com>
2023-12-08 15:32:31 +0700

    Completed : ex1,2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,26 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
+  let cleantext = "";
+  let lastIndex;
+  s = s.toLowerCase();
+
+  for (let i of s) {
+    if (i.charCodeAt(0) >= 97 && i.charCodeAt(0) <= 122) {
+      cleantext += i;
+    }
+  }
+
+  lastIndex = cleantext.length - 1;
+  console.log(cleantext);
+
+  for (i = 0; i < lastIndex / 2; i++) {
+    if (cleantext[i] != cleantext[lastIndex - i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
